0fa0b981f1983d02d8a3ece345218fd4a518e5fb
q0r3y <55005687+q0r3y@users.noreply.github.com>
2024-11-24 08:30:23 -0500

    Added worker code and wrangler.toml config

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+	async scheduled(event, env) {
+		const { zoneId, recordId, authToken, vpnNodesDb, cloudflareEmail } = env;
+		const cloudflareUrl = `https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records/${recordId}`;
+		const queryRandomAddress = `SELECT * FROM protonVpnNodesUs ORDER BY RANDOM() LIMIT 1`;
+		const vpnNodeUs = await vpnNodesDb.prepare(queryRandomAddress).first();
+		const dateTime = new Date().toISOString();
+
+		const requestData = {
+			comment: `${dateTime}: Worker updated node address.`,
+			name: 'us.protonvpn',
+			proxied: false,
+			settings: {},
+			tags: [],
+			ttl: 60,
+			content: vpnNodeUs.ipAddress,
+			type: 'A',
+		};
+
+		const headers = {
+			'Content-Type': 'application/json',
+			'X-Auth-Email': cloudflareEmail,
+			Authorization: `Bearer ${authToken}`,
+		};
+
+		await fetch(cloudflareUrl, {
+			method: 'PATCH',
+			headers: headers,
+			body: JSON.stringify(requestData),
+		})
+			.then((response) => response.json())
+			.then((data) => console.log('Success:', data))
+			.catch((error) => console.error('Error:', error));
 	},
 };
